5ffe82c286125ef5abc95ec1a136e7b646fda4aa
kkhanx <kulsumk2005@gmail.com>
2026-02-19 11:14:17 -0500

    completed main login page UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,31 +3,50 @@
 @section('title', 'Login')
 
 @section('content')
+<header>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/books.css') }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</header>
+
 <div id="login-page">
     <h1>Welcome Back!</h1>
-    <div class="login-container">
+
+    <h3>Log in to continue</h3>
 
         <form method="POST" action="{{ route('login.post') }}" class="login-form-card">
+
             @csrf
             
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus>
-            @error('email')
-                <span class="error-text">{{ $message }}</span>
-            @enderror
+            <div class="login-info">
+                <label for="email">UWindsor Email Address</label>
+                <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus>
+                @error('email')
+                    <span class="error-text">{{ $message }}</span>
+                @enderror
 
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-            @error('password')
-                <span class="error-text">{{ $message }}</span>
-            @enderror
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+                @error('password')
+                    <span class="error-text">{{ $message }}</span>
+                @enderror
 
-            <label class="remember-me">
-                <input type="checkbox" name="remember" value="1"> Remember me
-            </label>
+                <label class="remember-me">
+                    <input type="checkbox" name="remember" value="1">Remember Me</input>
+                </label>
 
-            <button type="submit">Login</button>
+                <button type="submit" class="loginbtn">Login</button>
+
+            </div>
+
+            <div class="login-footer">
+                <span class="signup-link"><a href="{{ route('register') }}">Sign Up</a></span>
+                <span class="fpsw"><a href="#">Forgot password?</a></span>
+            </div>
+
+
         </form>
-    </div>
+
 </div>
 @endsection
