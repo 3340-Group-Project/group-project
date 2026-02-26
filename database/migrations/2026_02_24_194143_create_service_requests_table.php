2b996131f734c5be8897aed495013940d6da776b
rbrueda <rbrueda8@gmail.com>
2026-02-26 13:36:55 -0500

    Changed to appropriate name in DB schema

--- database/migrations/2026_02_24_194143_create_service_requests_table.php
+++ database/migrations/2026_02_24_194143_create_service_requests_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('subject');
             $table->text('message');
-            $table->string('attachment_image_path')->nullable();
+            $table->string('attachment_path')->nullable();
             $table->timestamps();
         });
     }
