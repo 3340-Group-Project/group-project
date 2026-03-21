4a81522efd052cd8f6f5b615f48fa9318cdb5531
kkhanx <kulsumk2005@gmail.com>
2026-03-21 18:36:52 -0400

    added initial comments + fixed some signup UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,10 @@
+<!-- php file that will allow an existing user to sign in -->
+
 @extends('layouts.app')
 
 @section('title', 'Login')
 
+<!-- set the styling to form.css -->
 @section('styles')
     <link rel="stylesheet" href="{{ asset('css/form.css') }}"/>
 @endsection
@@ -12,6 +15,7 @@
 
 @section('content')
 <header>
+    <!-- meta tag instructs browsers to scale to screen size. it is critical for mobile-first indexing and SEO -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </header>
 
@@ -21,10 +25,12 @@
     <h3>Log in to continue</h3>
 
         <form method="POST" action="{{ route('login.post') }}" class="user-form-card" id="login-form">
-
+            <!-- @csrf used to prevent cross-site requests and malicious attacks -->
             @csrf
             
             <div class="login-info">
+
+                <!-- user email input -->
                 <label for="email">UWindsor Email Address</label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus>
 
@@ -49,8 +55,7 @@
             </div>
 
             <div class="user-info-footer">
-                <span class="signup-link"><a href="{{ route('register') }}">Sign Up</a></span>
-                <span class="fpsw"><a href="#">Forgot password?</a></span>
+                <span class="signup-link">No account? <a href="{{ route('register') }}">Sign Up</a></span>
             </div>
 
 
